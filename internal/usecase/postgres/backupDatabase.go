e0da7008b3002fdc1982bdb19d8a4326bd8cc52e
evilbebra <69915802+evilbebra@users.noreply.github.com>
2023-05-15 20:14:57 +0500

    Refactor backupDatabase.go

--- internal/usecase/postgres/backupDatabase.go
+++ internal/usecase/postgres/backupDatabase.go
@@ -22,43 +22,23 @@ func (p *pgConnBackup) BackupDatabase(cfg config.Config) error {
 	scanner.Scan()
 	dbname := scanner.Text()
 
-	cmd := exec.Command("pg_dump",
-		"-h", cfg.Host,
-		"-p", strconv.Itoa(cfg.Port),
-		"-U", cfg.Username,
-		"-d", dbname)
-
 	fmt.Println("Введите путь, по которому сохранить бэкап (.dump):")
 	reader := bufio.NewReader(os.Stdin)
 	var filepath string
 	fmt.Fscan(reader, &filepath)
-	file, err := os.Create(filepath)
-	if err != nil {
-		return fmt.Errorf("failed to create backup file: %v", err)
-	}
-	defer file.Close()
 
-	cmd.Stdout = file
+	cmd := exec.Command("pg_dump",
+		"-h", cfg.Host,
+		"-p", strconv.Itoa(cfg.Port),
+		"-U", cfg.Username,
+		"-d", dbname,
+		"-f", filepath)
 
-	stdin, err := cmd.StdinPipe()
+	err := cmd.Run()
 	if err != nil {
-		fmt.Println(err, "stdinPipe")
-		return err
-	}
-	defer stdin.Close()
-
-	// запускаем команду
-	err = cmd.Start()
-	if err != nil {
-		fmt.Println(err, "start cmd")
-		return err
+		return fmt.Errorf("run cmd %v", err)
 	}
 
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Println(err, "wait cmd")
-		return err
-	}
 	fmt.Printf("Бэкап %s был создан по пути: %s \n", dbname, filepath)
 	return nil
 }
